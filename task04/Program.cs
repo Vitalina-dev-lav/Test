c87aa2cdf35b3bffa4159b29b2f6fb55d812e0e2
Vitalina <vitalinabatrakova@gmail.com>
2022-11-18 16:59:58 +0300

    Task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -4,19 +4,19 @@
 // 44, 5, 78 -> 78
 // 22, 3, 9 -> 22
 
-Console.Clear();
-Console.WriteLine("Введите первое число:");
-int Num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
-int Num2 = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите третье число:");
-int Num3 = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 
-if (Num1<Num2) Console.WriteLine($"{Num2}");
-else if (Num2<Num3) Console.WriteLine($"{Num3}");
-else if (Num3<Num1) Console.WriteLine($"{Num1}");
+int max = Math.Max(a, Math.Max(b, c));
+Console.WriteLine(max);
+
+
 
 
 
