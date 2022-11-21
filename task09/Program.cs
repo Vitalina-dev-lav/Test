d85ad63e73df174c240469bb71b83503131fa7ca
Vitalina <vitalinabatrakova@gmail.com>
2022-11-22 01:27:54 +0300

    task11

--- task09/Program.cs
+++ task09/Program.cs
@@ -1,19 +1,28 @@
-﻿// // Задача 9. Напишите программу, которая 
-// 1.выводит случайное число из отрезка [10,99]
-// // и 
+﻿// Задача 9. Напишите программу, которая 
+// 1.выводит случайное число из отрезка [10,99] и 
 // 2. показывает наибольшую цифру числа.
 
-// // 78 -> 8
-// // 12 -> 2
-// // 85 -> 8
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
 
 
-int number = new Random().Next(10, 100);
-Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}");
+// int number = new Random().Next(10, 100);
+// Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}");
 
-int firstDigit = number / 10; //7
-int secondDigit = number % 10; //8
+// int firstDigit = number / 10; //7
+// int secondDigit = number % 10; //8
 
-if (firstDigit > secondDigit)Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
 
+int MaxDigit(int num) // int num = number
+{
+    int firstDigit = num / 10; //7
+    int secondDigit = num % 10; //8
+    if (firstDigit > secondDigit) return firstDigit;
+    return secondDigit;
+}
+
+int maxDigit = MaxDigit(num);
+Console.WriteLine($"Наибольшая цифра числа --> {maxDigit}");
