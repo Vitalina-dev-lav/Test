768208f6780bbc288b45e1fa9f372b3b9c1221d0
Vitalina <vitalinabatrakova@gmail.com>
2022-12-01 00:11:32 +0300

    task9

--- task09/Program.cs
+++ task09/Program.cs
@@ -7,28 +7,12 @@
 // 85 -> 8
 
 
-// int number = new Random().Next(10, 100);
-// Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}");
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}");
 
-// int firstDigit = number / 10; //7
-// int secondDigit = number % 10; //8
+int firstDigit = number / 10; //7
+int secondDigit = number % 10; //8
 
-// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
-// else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int MaxDigit(int num) // int num = number
-        {
-            int firstDigit = num / 10; //7
-            int secondDigit = num % 10; //8
-            if (firstDigit > secondDigit) return firstDigit;
-            return secondDigit;
-        }
-
-        int maxDigit = MaxDigit(num);
-        Console.WriteLine($"Наибольшая цифра числа --> {maxDigit}");
-    }
-}
