fa5689387cbecb7fac64878062e925db0611945f
Vitalina <vitalinabatrakova@gmail.com>
2022-11-30 17:52:14 +0300

    task18

--- task17/Program.cs
+++ task17/Program.cs
@@ -11,14 +11,16 @@ Console.WriteLine("Введите координаты точки");
 Console.Write("Y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-int Quarter (int xc, int yc)
+int Quarter (int xc,int yc)
 {
-if ( xc > 0 && yc > 0 )return 1;
-if ( xc < 0 && yc > 0 )return 2;
-if ( xc < 0 && yc < 0 )return 3;
-if ( xc > 0 && yc < 0 )return 4;
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
 }
 
 int quarter = Quarter(x, y);
-string result = quater > 0
- ? $"Указанные координаты соотвествует четверти -> {quater}
+string result = quarter > 0
+     ? $"Указанные координаты соотвествует четверти -> {quarter}"
+     : "Введены некоректные координаты";
+Console.WriteLine(result);
