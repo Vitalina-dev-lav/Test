83e07bd187d0721e179218ae54a5d71e0a9c3c4b
Vitalina <vitalinabatrakova@gmail.com>
2022-12-11 14:10:35 +0300

    task31

--- task31/Program.cs
+++ task31/Program.cs
@@ -5,47 +5,47 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
 // положительных чисел равна 29, сумма отрицательных равна -20.
 
-int[] CreateArrayRndInt (int size, int min,int max)
+int[] CreateArrayRndInt(int size, int min, int max)
 {
-   int[] arr = new int[size];
-   Random rnd = new Random();
+    int[] arr = new int[size];
+    Random rnd = new Random();
 
-   for (int i =0; i < arr.Length; i++)
-   {
-    arr[i] = rnd.Next(min, max +1);
-   }
-   return arr;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
 }
 
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
-   Console.Write("[");
-   for (int i = 0; i < arr.Length; i++)
-   { 
-    if(i < arr.Length - 1) Console.Write($"{arr[i]},");
-    else Console.Write($"{arr[i]}");
-   }
-   Console.WriteLine("]");
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
 }
 
-int GetSumNegativeElem (int[] arr)
+int GetSumNegativeElem(int[] arr)
 {
-   int sum =0;
-   for (int i = 0; i < arr.Length; i++)
-   {
-      if (arr[i] < 0) sum += arr[i]; //sumNegativ = sumNegativ + arr[i]
-   }
-   return sum;
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0) sum += arr[i]; //sumNegativ = sumNegativ + arr[i]
+    }
+    return sum;
 }
 
-int GetSumPositiveElem (int[] arr)
+int GetSumPositiveElem(int[] arr)
 {
-   int sum =0;
-   for (int i = 0; i < arr.Length; i++)
-   {
-      if (arr[i] > 0) sum += arr[i]; //sumPositive = sumPositive + arr[i]
-   }
-   return sum;
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum += arr[i]; //sumPositive = sumPositive + arr[i]
+    }
+    return sum;
 }
 
 
