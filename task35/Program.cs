b81864ebeca45b61d012ffd746ed7352cca9ef92
Vitalina <vitalinabatrakova@gmail.com>
2022-12-12 18:56:30 +0300

    task37

--- task35/Program.cs
+++ task35/Program.cs
@@ -8,3 +8,46 @@
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 9 && arr[i] < 100) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(123, 10, 99);
+PrintArray(array);
+CountElements(array);
+int countElements = CountElements(array);
+
+Console.WriteLine($"Количество двухзначных элементов массива равно {countElements}");
+
+
+
