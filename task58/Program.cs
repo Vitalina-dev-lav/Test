cf74eb7b29f5a08aff4e33fb9f32e92b697bb2fa
Vitalina <vitalinabatrakova@gmail.com>
2022-12-16 18:59:52 +0300

    task58

--- task58/Program.cs
+++ task58/Program.cs
@@ -1,5 +1,4 @@
-﻿// Задача 58: 1.Задайте две матрицы. 
-// 2.Напишите программу, которая будет находить произведение двух матриц.
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
 // 2 4 | 3 4
 // 3 2 | 3 3
@@ -7,78 +6,54 @@
 // 18 20
 // 15 18
 
-int[,] CreateMatrix1 (int row, int col, int min, int max) 
+int[,] RandomMatrix(int rows, int columns, int min, int max)
 {
-    int[,] matrix1 = new int[row, col];
-    Random rnd = new Random();   
-    for (int i = 0; i < matrix1.GetLength(0); i++) 
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix1.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix1[i,j] = rnd.Next(min, max +1);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
-    return matrix1;
-} 
- 
-int[,] CreateMatrix2 (int row, int col, int min, int max) 
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string sep1, string sep2)
 {
-    int[,] matrix2 = new int[row, col];
-    Random rnd = new Random();   
-    for (int i = 0; i < matrix2.GetLength(0); i++) 
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix2.GetLength(1); j++)
+        Console.Write($"{sep1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix2[i,j] = rnd.Next(min, max +1);
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine($"{sep2}");
+    }
+}
+
+int[,] MatrixMultiplication(int[,] firstmatrix, int[,] secondmatrix, int rows, int columns)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < firstmatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondmatrix.GetLength(1); j++)
+        {
+            resultMatrix[i, j] = 0;
+            for (int k = 0; k < firstmatrix.GetLength(1); k++)
+            {
+                resultMatrix[i, j] = resultMatrix[i, j] + firstmatrix[i, k] * secondmatrix[k, j];
+            }
         }
     }
-    return matrix2;
-} 
- 
-void PrintMatrix1(int[,] matrix)
-{
-    for (int i = 0; i <  matrix.GetLength(0); i++)
-    {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++) 
-        {
-            if (j < matrix.GetLength(1) -1) Console.Write($"{matrix[i, j], 1}|");
-            else Console.Write($"{matrix[i, j], 1}");
-        }
-        Console.WriteLine("|");        
-    }        
+    return resultMatrix;
 }
- 
-void PrintMatrix2(int[,] matrix)
-{
-    for (int i = 0; i <  matrix.GetLength(0); i++)
-    {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++) 
-        {
-            if (j < matrix.GetLength(1) -1) Console.Write($"{matrix[i, j], 1}|");
-            else Console.Write($"{matrix[i, j], 1}");
-        }
-        Console.WriteLine("|");        
-    }        
-}
- 
-void DivMatrix(int[,] matrix1, int[,] matrix2)
-{
-    int aRows = matrix1.Length[0]; int aCols = matrix1[1].Length;
-    int bRows = matrix2.Length[0]; int bCols = matrix2[1].Length;
-    if (aCols != bRows)    
-    double[][] result = MatrixCreate(aRows, bCols);
-    for (int i = 0; i < aRows; i++) 
-        for (int j = 0; j < bCols; j++) 
-        for (int k = 0; k < aCols; k++)
-        result[i][j] += matrix1[i][k] * matrix2[k][j];
-    return result;
-}
- 
-int[,] array2D = CreateMatrix1(2, 2, 0, 9);
-int[,] matrix = CreateMatrix2(2, 2, 0, 9);
-PrintMatrix1(array2D);
+
+int[,] array2D = RandomMatrix(2, 2, 0, 9);
+int[,] secondArray2D = RandomMatrix(2, 2, 0, 9);
+PrintMatrix(array2D, "|", "|");
 Console.WriteLine();
-PrintMatrix2(array2D);
-DivMatrix();
+PrintMatrix(secondArray2D, "|", "|");
+Console.WriteLine();
