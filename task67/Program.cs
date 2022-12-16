230366868001d0238063a7ce76190f57f94b772f
Vitalina <vitalinabatrakova@gmail.com>
2022-12-16 19:42:28 +0300

    task69

--- task67/Program.cs
+++ task67/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите первое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigts(int numb)
+{
+    return numb != 0 ? numb % 10 + SumDigts(numb / 10) : 0;
+}
+
+Console.WriteLine(SumDigts(Math.Abs(number)));
+
--- task69/Program.cs
+++ task69/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для возведения в степень первого числа");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int PowerRecMath (int a, int n)
+{   
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath (a * a, n / 2);
+    else return PowerRecMath (a, n - 1) * a;
+}
+
+int result = PowerRecMath(num1, num2);
+Console.WriteLine(result);
