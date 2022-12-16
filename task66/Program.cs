29da2343e0b3c97a819054b35d63e9c1e69344e3
Vitalina <vitalinabatrakova@gmail.com>
2022-12-16 19:48:16 +0300

    task66'

--- task66/Program.cs
+++ task66/Program.cs
@@ -4,3 +4,17 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int SumOfEllements (int a, int b)
+{ 
+    if (a == b) return b;
+    return SumOfEllements(a + 1, b) + a;
+}
+
+int result = SumOfEllements(num1, num2);
+Console.WriteLine(result);
