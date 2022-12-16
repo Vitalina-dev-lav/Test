1aa1722b317fb2fe601d00eddb7281f54571345e
Vitalina <vitalinabatrakova@gmail.com>
2022-12-16 19:45:46 +0300

    task64

--- task64/Program.cs
+++ task64/Program.cs
@@ -5,3 +5,14 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(num);
+
+void NaturalNumbers (int number)
+{
+    if (number == 0) return;
+    Console.Write($"{number}, ");
+    NaturalNumbers (number-1 );
+}
