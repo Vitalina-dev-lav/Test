77f4bb444fdf0dd9237440d4fd4c346c9453e1c5
Vitalina <vitalinabatrakova@gmail.com>
2022-12-16 19:50:25 +0300

    task68

--- task68/Program.cs
+++ task68/Program.cs
@@ -3,3 +3,19 @@
 // 3.Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Akerman (int a, int b)
+{ 
+    if (a == 0) return b + 1;
+    else if (b==0) return Akerman (a-1, 1);
+    return Akerman( a - 1, Akerman (a, b-1));
+}
+
+int result = Akerman(num1, num2);
+Console.WriteLine(result);
